7126184e9fefa04d0e1557eec64535f114bd4c58
lixinyi-dut <lxy_September@outlook.com>
2017-10-10 20:40:57 +0800

    first 4

--- programming languages/hw4.rkt
+++ programming languages/hw4.rkt
@@ -5,3 +5,22 @@
 
 ;; put your code below
 
+(define (sequence low high stride)
+   (if (> low high)
+       null
+       (cons low (sequence (+ low stride) high stride))))
+
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix) ) xs))
+
+(define (list-nth-mod xs n)
+  (if (< n 0) (error "list-nth-mod: negative number")
+      (if (null? xs)
+          (error "list-nth-mod: empty list")
+          (car (list-tail xs (remainder n (length xs)))))) )
+
+(define (stream-for-n-steps s n)
+  (if (= n 0) null
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))))
+
+
