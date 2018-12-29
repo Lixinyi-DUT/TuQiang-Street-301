b5d145b01b031ae312f1b6ea122b49a4b7632c3c
Xinyi Li <wolixinyi@gmail.com>
2018-12-29 23:12:01 +0800

    pass auto-grader

--- programming languages/hw6assignment.rb
+++ programming languages/hw6assignment.rb
@@ -5,20 +5,14 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1],[-1,-1]]), # new type 1
+  All_My_Pieces = All_Pieces+
+               [rotations([[0, 0], [-1, 0], [1, 0], [0, -1],[-1,-1]]), # new type 1
                [[[0, 0], [-1, 0], [-2,0], [1, 0], [2, 0]], # new type 2, long
                [[0, 0], [0, -1], [0,-2],[0, 1], [0, 2]]],
-               [[[0, 0], [-1, 0], [0, -1]]]] # T
+               rotations([[0, 0], [-1, 0], [0, -1]])] # new type 3
 
   # your enhancements here
+
   def self.next_piece (board)
     MyPiece.new(All_My_Pieces.sample, board)
   end
@@ -27,6 +21,7 @@ end
 class MyBoard < Board
   # your enhancements here
   def initialize (game)
+    @cheated = false
     @grid = Array.new(num_rows) {Array.new(num_columns)}
     @current_block = MyPiece.next_piece(self)
     @score = 0
@@ -35,8 +30,32 @@ class MyBoard < Board
   end
 
   def next_piece
-    @current_block = MyPiece.next_piece(self)
+    if @cheated
+      @current_block =MyPiece.new([[[0,0]]],self)
+      @cheated=false
+    else
+      @current_block = MyPiece.next_piece(self)
+    end
     @current_pos = nil
+  end
+
+  def cheat
+    if @score>=100 and !@cheated
+      @score-=100
+      @cheated=true
+    end
+  end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..locations.size-1).each{|index|
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] =
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
   end
 end
 
@@ -48,5 +67,12 @@ class MyTetris < Tetris
     @canvas.place(@board.block_size * @board.num_rows + 3,
                   @board.block_size * @board.num_columns + 6, 24, 80)
     @board.draw
+  end
+
+  def key_bindings
+    super
+    @root.bind('u', proc {@board.rotate_clockwise;
+      @board.rotate_clockwise})
+    @root.bind('c', proc {@board.cheat})
   end
 end
